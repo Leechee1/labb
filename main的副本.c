e6a15b9a49da8e292d1e6fff65455cf3a3501aed
Leechee1” <leechee_yuan@163.com>
2021-10-10 20:56:04 +0800

    2.0

---
+++ main的副本.c
@@ -0,0 +1,175 @@
+//
+//  main.c
+//  practice lab
+//
+//  Created by 李志远 on 2021/10/9.
+//
+
+
+//找零计算器
+//#include <stdio.h>
+//
+//int main(){
+//    int A=0;
+//    int B=0;
+//    printf("请输入购买的金额");
+//    scanf("%d",&B);
+//
+//    printf("请输入你的现金");
+//    scanf("%d",&A);
+//    int C=A-B;
+//
+//    if (C>0) {printf("找你的零钱是%d",C);
+//
+//
+//    }
+//        else
+//            printf("对不起，你的钱不够");
+//
+//
+//
+//    return 0;
+//
+//
+//
+//}
+
+
+
+//switch case 语句
+
+//#include "stdio.h"
+//int main()
+//{
+//
+//
+//    int month=0;
+//    printf("please enter the number of the month");
+//    scanf("%d",&month);
+//    switch (month) {
+//        case 1:
+//            printf("January");
+//            break;
+//        case 2:
+//            printf("February");
+//            break;
+//        case 3:
+//            printf("March");
+//            break;
+//
+//        default:
+//            printf("sorry,i don't know \n");
+//
+//            }
+//
+//    return 0;
+//
+//}
+
+
+
+//计数循环程序
+
+//#include "stdio.h"
+//
+//int main(){
+//
+//    int count=100;
+//
+////Way 1
+////    do {
+////        printf("%d\n",count);
+////        count--;
+////    } while (count>0);
+////    printf("Lauching!!");
+////
+//
+////Way 2
+//    while (count>0) {
+//        printf("%d\n",count);
+//        count--;
+//    }
+//
+//    printf("Launching!!");
+//
+//
+//
+//}
+
+
+//计数游戏
+
+//#include "stdio.h"
+//#include "stdlib.h"
+//#include "time.h"
+//
+//int main(){
+//
+//
+//    srand(time(0));
+//    int a=rand();
+//    int b=0;
+//    int c=0;
+////    printf("%d\n",a%100);
+//    printf("please enter your number\n");
+//    scanf("%d",&b);
+//    int d=a%100;
+//    while (b!=d) {
+//        c++;
+//        printf("你猜错了，请再输入一个数字\n");
+//        scanf("%d",&b);
+//
+//    }
+//        printf("恭喜你，你猜对了！\n");
+//    printf("你用了%d次猜对了这个数",c);
+//
+//}
+    
+
+
+//计数游戏版本二
+
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+
+int main(){
+    
+    
+    srand(time(0));
+    int a=rand()%100;
+    int b=0;
+    int c=0;
+   
+    
+    do{ printf("请输入一个100以内的数字：\n");
+        scanf("%d",&b);
+       if (b>a) {
+        
+           printf("你输入的数大了");}
+       else if (b<a){
+           printf("你输入的数小了");
+           
+       }
+        
+        c++;
+    }while (b!=a);
+   
+        printf("恭喜你，你用了%d次猜对了！！",c);
+    
+    return 0;
+
+}
+
+
+            
+    
+    
+    
+    
+    
+    
+
